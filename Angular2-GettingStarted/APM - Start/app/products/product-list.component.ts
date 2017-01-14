cccfb4d60c573909776a4c0c56e96e6b761d16e2
Esteban Guzmán Zerafín <eguzman@ia.com.mx>
2017-01-13 20:47:57 -0600

    new commit

---
+++ Angular2-GettingStarted/APM - Start/app/products/product-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit }from '@angular/core';
+import {IProduct }from './product';
+import {ProductService} from './product.service';
+
+@Component( {
+    selector:'pm-products', 
+    moduleId:module.id, 
+    templateUrl:'product-list.component.html', 
+    styleUrls:['product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit {
+    pageTitle:string = 'Product List'; 
+    listFilter:string = 'cart'; 
+    showImage:boolean = false; 
+    imageWidth:number = 50; 
+    imageMargin:number = 2; 
+
+    products:IProduct[]; 
+
+    constructor(private _productService : ProductService){
+
+    }
+
+    toggleImage():void {
+        this.showImage =  ! this.showImage; 
+    }
+
+    ngOnInit():void {
+        this.products = this._productService.getProducts();
+    }
+
+    onRatingClicked(message:string):void {
+        this.pageTitle = 'Product List: ' + message; 
+    }
+}
